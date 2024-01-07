53d2c248f61532db4adb09271d835c0ba00cdce9
stefanovagloria <gloriqstefanova97@gmail.com>
2024-01-07 21:50:51 +0300

    Components added

---
+++ src/components/contacts/Contacts.jsx
@@ -0,0 +1,8 @@
+import styles from './Contacts.module.css';
+
+export default function Contacts(){
+
+    return(
+        <div>Contacts Page</div>
+    )
+}
---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,8 @@
+import styles from './Gallery.module.css';
+
+export default function Gallery(){
+
+    return(
+        <div>Gallery Page</div>
+    )
+}
