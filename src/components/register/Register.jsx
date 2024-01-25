65d4b3895087f56d189b943bb49baae787d0d91b
stefanovagloria <gloriqstefanova97@gmail.com>
2024-01-25 09:51:51 +0300

    Register form added

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -2,6 +2,27 @@ import styles from './Register.module.css';
 
 export default function Register(){
     return(
-        <h1>Register Page</h1>
+        <form>
+        <div className={styles.container}>
+          <h1>Register</h1>
+          <p>Please fill in this form to create an account.</p>
+      
+          <label htmlFor="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" id="email" required></input>
+      
+          <label htmlFor="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" id="psw" required></input>
+      
+          <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
+          <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required></input>
+      
+          <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+          <button type="submit" className={styles.registerbtn}>Register</button>
+        </div>
+      
+        <div className="container signin">
+          <p>Already have an account? <a href="#">Sign in</a>.</p>
+        </div>
+      </form> 
     )
-}
+};
