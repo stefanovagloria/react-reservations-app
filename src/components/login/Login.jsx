3938a90dfd8974cb924f69bd942f51eb4f798086
stefanovagloria <gloriqstefanova97@gmail.com>
2024-01-26 20:17:27 +0300

    Controlled register form created

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,42 @@
-import styles from './Login.module.css';
+import styles from "./Login.module.css";
 
-export default function Login(){
-    return(
-        <h1>Login Page</h1>
-    )
+export default function Login() {
+  return (
+    <form>
+      <div className={styles.container}>
+        <h1>Login</h1>
+
+        <label htmlFor="email">
+          <b>Email</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          id="email"
+          required
+        ></input>
+
+        <label htmlFor="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          id="psw"
+          required
+        ></input>
+        <button type="submit" className={styles.registerbtn}>
+          Login
+        </button>
+      </div>
+
+      <div className="container signin">
+        <p>
+          Already have an account? <a href="#">Sign in</a>.
+        </p>
+      </div>
+    </form>
+  );
 }
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,28 +1,81 @@
-import styles from './Register.module.css';
+import { useState } from "react";
+import styles from "./Register.module.css";
 
-export default function Register(){
-    return(
-        <form>
-        <div className={styles.container}>
-          <h1>Register</h1>
-          <p>Please fill in this form to create an account.</p>
-      
-          <label htmlFor="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" id="email" required></input>
-      
-          <label htmlFor="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" id="psw" required></input>
-      
-          <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-          <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required></input>
-      
-          <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-          <button type="submit" className={styles.registerbtn}>Register</button>
-        </div>
-      
-        <div className="container signin">
-          <p>Already have an account? <a href="#">Sign in</a>.</p>
-        </div>
-      </form> 
-    )
-};
+export default function Register() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    rePassword: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setValues(values => ({...values, [e.target.name]: e.target.value }))
+  };
+
+  const onSubmitHandler = (e) =>{
+    e.preventDefault();
+
+    // api call register
+  }
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <div className={styles.container}>
+        <h1>Register</h1>
+
+        <label htmlFor="email">
+          <b>Email</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          id="email"
+          value={values.email}
+          onChange={onChangeHandler}
+          required
+        ></input>
+
+        <label htmlFor="password">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          id="psw"
+          value={values.password}
+          onChange={onChangeHandler}
+          required
+        ></input>
+
+        <label htmlFor="rePassword">
+          <b>Repeat Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          name="rePassword"
+          id="psw-repeat"
+          value={values.rePassword}
+          onChange={onChangeHandler}
+          required
+        ></input>
+
+        <p>
+          By creating an account you agree to our{" "}
+          <a href="#">Terms & Privacy</a>.
+        </p>
+        <button type="submit" className={styles.registerbtn}>
+          Register
+        </button>
+      </div>
+
+      <div className="container signin">
+        <p>
+          Already have an account? <a href="#">Sign in</a>.
+        </p>
+      </div>
+    </form>
+  );
+}
