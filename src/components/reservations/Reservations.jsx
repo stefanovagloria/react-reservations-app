05653df1b615b6de62e67b242733ef7030de3655
stefanovagloria <gloriqstefanova97@gmail.com>
2024-02-01 20:11:47 +0300

    Booking form added to Reservations component

--- src/components/reservations/Reservations.jsx
+++ src/components/reservations/Reservations.jsx
@@ -1,5 +1,49 @@
-export default function Reservations(){
-    return(
-        <h1>Reservations page</h1>
-    )
+import image from "./../../assets/booking2.jpg";
+import styles from "./Reservations.module.css";
+
+export default function Reservations() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.img}>
+        <img src={image} />
+      </div>
+      <div className={styles.bookingForm}>
+        <h1>BOOK A ROOM</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A sit pam
+          dolorem quos expedita, odio quibusdam esse ducimus deleniti nulla
+          minus omnis totam tempore cupiditate.
+        </p>
+        <form>
+          <div className={styles.labels}>
+            <label>Choose a date</label>
+            <input />
+            <label>Number of guests</label>
+            <input />
+          </div>
+          <div className={styles.labels}>
+            <label>Choose a room</label>
+            <input />
+            <label>Name</label>
+            <input />
+          </div>
+          <div className={styles.labels}>
+            <label>Telephone</label>
+            <input />
+            <label>Email</label>
+            <input />
+          </div>
+
+          <div className={styles.labels}>
+            <label>Preffered payment method</label>
+            <input />
+          </div>
+
+          <div>
+            <input type="submit" value="Send request" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
