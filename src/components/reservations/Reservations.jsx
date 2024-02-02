d47370ab116be8c33c9d785a1d544719f44c345b
stefanovagloria <gloriqstefanova97@gmail.com>
2024-02-02 08:28:37 +0300

    HTML Form in Reservations component changed from uncontrolled to controlled component and useForm hook added

--- src/components/reservations/Reservations.jsx
+++ src/components/reservations/Reservations.jsx
@@ -1,7 +1,26 @@
 import image from "./../../assets/booking2.jpg";
 import styles from "./Reservations.module.css";
+import { useForm } from "../../hooks/useForm";
 
 export default function Reservations() {
+  const {values, onChangeHandler, onSubmit} = useForm(
+    {
+      date: "",
+      guests: "",
+      roomType: "",
+      name: "",
+      telephone: "",
+      email: "",
+      paymentMethod: "",
+    },
+    onSubmitHandler
+  );
+
+  function onSubmitHandler(e) {
+    console.log("Sending request..");
+    // send post request to db
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.img}>
@@ -14,33 +33,72 @@ export default function Reservations() {
           dolorem quos expedita, odio quibusdam esse ducimus deleniti nulla
           minus omnis totam tempore cupiditate.
         </p>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className={styles.labels}>
-            <label>Choose a date</label>
-            <input />
-            <label>Number of guests</label>
-            <input />
+            <label htmlFor="date">Choose a date</label>
+            <input
+              name="date"
+              id="date"
+              value={values.date}
+              onChange={onChangeHandler}
+            />
+            <label htmlFor="guests">Number of guests</label>
+            <input
+              name="guests"
+              id="guests"
+              value={values.guests}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className={styles.labels}>
-            <label>Choose a room</label>
-            <input />
-            <label>Name</label>
-            <input />
+            <label htmlFor="roomType">Choose a room</label>
+            <input
+              name="roomType"
+              id="roomType"
+              value={values.roomType}
+              onChange={onChangeHandler}
+            />
+            <label htmlFor="name">Name</label>
+            <input
+              name="name"
+              id="name"
+              value={values.name}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className={styles.labels}>
-            <label>Telephone</label>
-            <input />
-            <label>Email</label>
-            <input />
+            <label htmlFor="telephone">Telephone</label>
+            <input
+              name="telephone"
+              id="telephone"
+              value={values.telephone}
+              onChange={onChangeHandler}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              id="email"
+              value={values.email}
+              onChange={onChangeHandler}
+            />
           </div>
 
           <div className={styles.labels}>
-            <label>Preffered payment method</label>
-            <input />
+            <label htmlFor="paymentMethod">Preffered payment method</label>
+            <input
+              name="paymentMethod"
+              id="paymentMethod"
+              value={values.paymentMethod}
+              onChange={onChangeHandler}
+            />
           </div>
 
           <div>
-            <input type="submit" value="Send request" />
+            <input
+              className={styles.sendBtn}
+              type="submit"
+              value="Send request"
+            />
           </div>
         </form>
       </div>
